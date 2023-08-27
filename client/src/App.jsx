80a46a66e419bdd1985ab95101ba95cbaa9e697f
“ZiiadkhanlyZZ”~ <zaur.ziiadkhanly~>
2023-08-27 22:47:08 +0400

    create react js and config bootstrap

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <div className="app">
+      <div className="container">
+        <h1>Sharetrade</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
